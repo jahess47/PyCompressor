040a12f9821b2bbe198ecf54a5bf46ea446bf9fe
jahess47 <jahess150@gmail.com>
2018-09-20 09:45:58 -0400

    Revised formatting and comments

--- bin/Main.py
+++ bin/Main.py
@@ -5,8 +5,13 @@ c = Compressor()
 
 # Run until user exits program
 while True:
-    choice = input("Please enter one of the following options:\n(1) Compress string\n(2) Decompress string\n(3) Exit\n")
+    # Get input from user
+    choice = input("Please enter one of the following options:\n"
+                   "(1) Compress string\n"
+                   "(2) Decompress string\n"
+                   "(3) Exit\n")
 
+    # Call the Compressor class based on the user's input
     if choice is "1":
         c.compress()
     elif choice is "2":
@@ -15,4 +20,5 @@ while True:
         print("Exiting PyCompressor...")
         exit()
     else:
+        # Catch any input that doesn't match 1, 2, or 3
         print("That is not a valid option.")
